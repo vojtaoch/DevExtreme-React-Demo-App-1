d3136bd6fccba6fdc574c6b8d3d8866e09ff00de
Vojtěch Ochmanský <ujtagr@seznam.cz>
2026-08-20 10:57:01 +0200

    add a DataGrid with some basic features from the provided demos and tutorials

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,79 @@
-import 'devextreme/dist/css/dx.common.css';
-import './themes/generated/theme.base.dark.css';
-import './themes/generated/theme.base.css';
-import './themes/generated/theme.additional.dark.css';
-import './themes/generated/theme.additional.css';
-import { HashRouter as Router } from 'react-router-dom';
-import './dx-styles.scss';
-import LoadPanel from 'devextreme-react/load-panel';
-import { NavigationProvider } from './contexts/navigation';
-import { AuthProvider } from './contexts/auth';
-import { useAuth } from './contexts/auth-hooks';
-import { useScreenSizeClass } from './utils/media-query';
-import Content from './Content';
-import UnauthenticatedContent from './UnauthenticatedContent';
-import { ThemeContext, useThemeContext} from "./theme";
+import React from 'react';
+import 'devextreme/dist/css/dx.fluent.blue.light.css';
+ 
+import { DataGrid, Editing, FilterRow, Sorting, TotalItem, GroupItem, Summary, Toolbar, Item, SearchPanel, type DataGridTypes, Export } from 'devextreme-react/data-grid';
+import samples from './employees.json';
 
+import { Workbook } from 'devextreme-exceljs-fork';
+import saveAs from 'file-saver';
+import { exportDataGrid } from 'devextreme/excel_exporter';
+import { jsPDF } from 'jspdf';
+import { exportDataGrid as exportDataGridToPdf} from 'devextreme/pdf_exporter';
+ 
 function App() {
-  const { user, loading } = useAuth();
+    return (
+        <div className="App">
+            <DataGrid
+                dataSource={samples}
+                keyExpr="id"
+                defaultColumns={['id', 'userId', 'title', 'body']}
+                onExporting={exportGrid}>
 
-  if (loading) {
-    return <LoadPanel visible={true} />;
-  }
+                  <Sorting mode="multiple" />
+                  <FilterRow visible={true} />
+                  <Editing
+                      mode="popup"
+                      allowUpdating={true}
+                      allowDeleting={true}
+                      allowAdding={true} />
 
-  if (user) {
-    return <Content />;
-  }
+                  <Summary>
+                    <TotalItem summaryType='count'
+                               column='id' />
+                    <GroupItem summaryType='count'
+                               column='userId' />
+                  </Summary>
 
-  return <UnauthenticatedContent />;
+                  <Toolbar>
+                    <Item name="addRowButton" showText="always" />
+                    <Item name="searchPanel" />
+                    <Item name="exportButton" />
+                  </Toolbar>
+
+                  <SearchPanel visible={true} />
+
+                  <Export enabled={true} formats={exportFormats} />
+                  
+            </DataGrid>
+        </div>
+    );
 }
 
-export default function Root() {
-  const screenSizeClass = useScreenSizeClass();
-  const themeContext = useThemeContext();
 
-  return (
-    <Router>
-      <ThemeContext.Provider value={themeContext}>
-        <AuthProvider>
-          <NavigationProvider>
-            <div className={`app ${screenSizeClass}`}>
-              <App />
-            </div>
-          </NavigationProvider>
-        </AuthProvider>
-      </ThemeContext.Provider>
-    </Router>
-  );
+const exportFormats = ['xlsx', 'pdf'];
+ 
+function exportGrid(e: DataGridTypes.ExportingEvent) {
+    if (e.format === 'xlsx') {
+        const workbook = new Workbook(); 
+        const worksheet = workbook.addWorksheet("Main sheet"); 
+        exportDataGrid({ 
+            worksheet: worksheet, 
+            component: e.component,
+        }).then(function() {
+            workbook.xlsx.writeBuffer().then(function(buffer) { 
+                saveAs(new Blob([buffer], { type: "application/octet-stream" }), "DataGrid.xlsx"); 
+            }); 
+        }); 
+    } else if (e.format === 'pdf') {
+        const doc = new jsPDF();
+        exportDataGridToPdf({
+            jsPDFDocument: doc,
+            component: e.component,
+        }).then(() => {
+            doc.save('DataGrid.pdf');
+        });
+    }
 }
+
+
+export default App;
